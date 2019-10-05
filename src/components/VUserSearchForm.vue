9c56b6982b5e58d9e462eff19eaf6702d08d7a61
Renan Paiva Bonette <renan.bonette@gmail.com>
2019-10-05 13:11:50 +0200

    first commit

---
+++ src/components/VUserSearchForm.vue
@@ -0,0 +1,12 @@
+
+<script>
+export default {
+  name: 'UserSearchForm'
+}
+</script>
+
+<template>
+  <div>
+    UserSearchForm
+  </div>
+</template>
---
+++ src/views/UserView.vue
@@ -0,0 +1,23 @@
+<script>
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
+export default {
+  name: 'UserView',
+  components: {
+    VUserSearchForm,
+    VUserProfile,
+  },
+  data() {
+    return {
+      user: { name: '' }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <VUserSearchForm />
+    <VUserProfile :user="user" />
+  </div>
+</template>
